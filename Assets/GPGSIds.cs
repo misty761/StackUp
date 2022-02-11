917e2ad4590558e509c72eb3a4616b75ff72931f
Ahndruid <55180559+misty761@users.noreply.github.com>
2022-02-11 10:34:55 +0900

    Update GPGSIds.cs

--- Assets/GPGSIds.cs
+++ Assets/GPGSIds.cs
@@ -24,7 +24,7 @@
 
 public static class GPGSIds
 {
-        public const string leaderboard_score = "CgkIwLWX3IMUEAIQAw"; // <GPGSID>
+        public const string leaderboard_score = "Cxxxxxxxxxxx"; // <GPGSID>
 
 }
 
